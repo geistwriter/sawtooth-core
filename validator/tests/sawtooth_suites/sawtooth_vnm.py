949085408ab186212210bd138a9b4ffc2e4ee6ec
Gregory Setterholm <gregoryx.setterholm@intel.com>
2016-11-25 10:21:45 +0000

    declarative
    Signed-off-by: Gregory Setterholm <gregoryx.setterholm@intel.com>
    
        --  create an indexed variant of a declarative node controller
        --  create IndexedVNM, the basis for an integration test concordant VNM,
            which uses:
            --  a wrapped node controller
            --  an indexed node command generator
        --  port hardening test to IndexedVNM to demonstrate utility

--- validator/tests/sawtooth_suites/sawtooth_vnm.py
+++ validator/tests/sawtooth_suites/sawtooth_vnm.py
@@ -12,69 +12,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ------------------------------------------------------------------------------
+
+import json
 import logging
-import time
 import traceback
 import unittest
 
 from integration.test_convergence import TestConvergence
-from sawtooth.manage.node import NodeArguments
-from sawtooth.manage.subproc import SubprocessNodeController
-from sawtooth.manage.wrap import WrappedNodeController
-from txnintegration.utils import Progress
-from txnintegration.utils import TimeOut
+from sawtooth.manage.declarative import get_default_vnm
 
 LOGGER = logging.getLogger(__name__)
 
+
 class SawtoothVnmTestSuite(unittest.TestCase):
     def _do_teardown(self):
-        print 'destroying', str(self.__class__.__name__)
-        if hasattr(self, '_node_ctrl') and self._node_ctrl is not None:
-            # Shut down the network
-            with Progress("terminating network") as p:
-                for node_name in self._node_ctrl.get_node_names():
-                    self._node_ctrl.stop(node_name)
-                to = TimeOut(16)
-                while len(self._node_ctrl.get_node_names()) > 0:
-                    if to.is_timed_out():
-                        break
-                    time.sleep(1)
-                    p.step()
-            # force kill anything left over
-            for node_name in self._node_ctrl.get_node_names():
-                try:
-                    print "%s still 'up'; sending kill..." % node_name
-                    self._node_ctrl.kill(node_name)
-                except Exception as e:
-                    print e.message
-            self._node_ctrl.clean()
+        if hasattr(self, 'vnm') and self.vnm is not None:
+            print 'destroying', str(self.__class__.__name__)
+            self.vnm.shutdown()
 
     def _do_setup(self):
-        # give defaults to teardown vars
-        self._node_ctrl = None
         print 'creating', str(self.__class__.__name__)
-        # set up our nodes (suite-internal interface)
-        self._nodes = [
-            NodeArguments('v%s' % i, 8800 + i, 9000 + i) for i in range(2)
-        ]
-        # set up our urls (external interface)
-        self.urls = ['http://localhost:%s' % x.http_port for x in self._nodes]
-        # Make genesis block
-        print 'creating genesis block...'
-        self._nodes[0].genesis = True
-        self._node_ctrl = WrappedNodeController(SubprocessNodeController())
-        self._node_ctrl.create_genesis_block(self._nodes[0])
-        # Launch network (node zero will trigger bootstrapping)
-        print 'launching network...'
-        for x in self._nodes:
-            self._node_ctrl.start(x)
+        self.vnm = get_default_vnm(2)
+        self.vnm.do_genesis(0)
+        self.vnm.launch()
 
     def test_suite(self):
         success = False
         try:
             print
             self._do_setup()
-            urls = self.urls
+            print json.dumps(self.vnm.sit_rep(err_on_fail=True), indent=4)
+            urls = self.vnm.urls()
             suite = unittest.TestSuite()
             suite.addTest(TestConvergence('test_bootstrap', urls))
             runner = unittest.TextTestRunner()
